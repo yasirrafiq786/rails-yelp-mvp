44269561a4a627005ab6bc850bb242610f99c236
Yasir Malik <yasirrafiq786@gmail.com>
2020-04-28 20:05:46 +1000

    made CRUD routes and added restaurant controller

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantController < ApplicationController
+  def name:string
+  end
+
+  def address:string
+  end
+
+  def phone_number:integer
+  end
+
+  def category:string
+  end
+end
